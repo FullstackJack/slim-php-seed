0f1d0023a4b447a6fc8bd09a346a0961eeb9f156
Kyle Bedell <kgbedell@gmail.com>
2015-11-19 19:00:58 -0800

    Added PHP dotenv to project.

--- public/index.php
+++ public/index.php
@@ -5,12 +5,31 @@ if(file_exists(__DIR__.'/../vendor/autoload.php')){
     require_once __DIR__.'/../vendor/autoload.php';
 }
 
+// Load Environment Variables
+$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
+$dotenv->load();
+
 // App Instance
 $app = new Slim\Slim(array(
     'templates.path' => '../views',
     'view' => new \Slim\Views\Twig(),
-    'debug' => true
 ));
+
+// Only invoked if mode is "production"
+$app->configureMode('production', function () use ($app) {
+    $app->config(array(
+        'log.enable' => true,
+        'debug' => false
+    ));
+});
+
+// Only invoked if mode is "development"
+$app->configureMode('development', function () use ($app) {
+    $app->config(array(
+        'log.enable' => false,
+        'debug' => true
+    ));
+});
 
 // Load Routes
 if(file_exists(__DIR__.'/../config/routes.php')){
